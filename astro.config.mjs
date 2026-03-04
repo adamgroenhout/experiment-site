9a5b9ae0c8234f5faef58d61fa20b78e017afdd6
adamgroenhout <10010658+adamgroenhout@users.noreply.github.com>
2026-03-04 04:58:00 +0000

    docs: add github pages URL to README

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.github.io',
-  base: '/my-repo',
+  site: 'https://adamgroenhout.github.io',
+  base: '/experiment-site',
 });
